446571e9e21e985a60bc09eadb551a36a36d8abe
FlavioBS <bzsflavio@gmail.com>
2021-11-28 20:24:17 -0300

    feature: start project

---
+++ src/auth/strategies/google.strategy.ts
@@ -0,0 +1,28 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Profile, Strategy } from 'passport-google-oauth20';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class GoogleOauthStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor(configService: ConfigService) {
+    super({
+      clientID: configService.get<string>('OAUTH_GOOGLE_ID'),
+      clientSecret: configService.get<string>('OAUTH_GOOGLE_SECRET'),
+      callbackURL: configService.get<string>('OAUTH_GOOGLE_REDIRECT_URL'),
+      scope: ['email', 'profile'],
+    });
+  }
+
+  async validate(
+    _accessToken: string,
+    _refreshToken: string,
+    profile: Profile,
+  ) {
+    const { id, name, emails } = profile;
+
+    const user = profile;
+
+    return user;
+  }
+}
---
+++ src/users/users.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { UsersRepository } from './user.repository';
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService, UsersRepository],
+  exports: [UsersRepository],
+})
+export class UsersModule {}
